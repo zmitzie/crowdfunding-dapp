7221697e959ce681d946b7865f2c77cc01033691
Zacharias Mitzelos <accounts@mitzelos.com>
2019-07-26 19:56:56 +0300

    Add comments in Tests

--- test/crowdfunding.test.js
+++ test/crowdfunding.test.js
@@ -26,33 +26,42 @@ contract('Crowdfunding', function(accounts) {
     })
 
     it("should not accept contribution from creator", async () => {
+      // should revert due to the required statement
       await catchRevert(campaignInstance.contribute({from: creator, value: amount}))
     });
 
     it("should not accept contribution for a successful campaign", async () => {
       await campaignInstance.contribute({from: alice, value: goal})
+      // should revert due to the required statement
       await catchRevert(campaignInstance.contribute({from: alice, value: amount}))
     });
 
     it("should increase campaign balance when a contribution is made", async () => {
+      // to properly log contributions and mark campaign as completed eventually
       await campaignInstance.contribute({from: alice, value: amount})
+      // check contract balance via getter method
       const newBalance = await campaignInstance.balance()
       assert.equal(newBalance, amount, 'Amount deposited is not reflected to balance of the contract')
     });
 
     it("should log contributors address and amount", async () => {
+      //if the contributor address is not logged then refund won't work if campaign expires
       await campaignInstance.contribute({from: alice, value: amount})
       const amountContributed = await campaignInstance.contributions(alice)
       assert.equal(amountContributed, amount, 'Mapping did not return the correct amount that the user deposited')
     });
 
     it("should mark campaign successful once the goal has been reached", async () => {
+      // if the state doesn't change the users can still contribute
+      // contributing the goal amount and then checking the state of contract
       await campaignInstance.contribute({from: alice, value: goal})
       const newState = await campaignInstance.state()
+      // 2 means Successful
       assert.equal(newState, 2, 'Campaign state not changed to successful')
     });
     
     it("should set balance to zero once the goal has been reached", async () => {
+      //two different contributions, eventually reaching the campaign goal and triggering the checkCampaignStatus()
       await campaignInstance.contribute({from: alice, value: halfGoal})
       await campaignInstance.contribute({from: alice, value: halfGoal})
 
@@ -61,10 +70,13 @@ contract('Crowdfunding', function(accounts) {
     });
 
     it("should transfer campaign balance to creator once the goal has been reached", async () => {
+      // get balance of creator before contributions made to the campaign
       let balanceAfterContractCreation =  await web3.eth.getBalance(creator);
+      // convert wei to eth, string to number and add amount that will be deposited
       let previousBalance = Number(web3.utils.fromWei(balanceAfterContractCreation)) + Number(web3.utils.fromWei(goal))
-      
+      // deposit goal amount - checkCampaignStatus() will run and transfer campaign balance to creator
       await campaignInstance.contribute({from: alice, value: goal})
+      // get new balance of creator
       let actualBalance = await web3.eth.getBalance(creator);
 
       assert.deepEqual(previousBalance, Number(web3.utils.fromWei(actualBalance)), "Campaign balance not transfered to creator");
@@ -72,6 +84,7 @@ contract('Crowdfunding', function(accounts) {
 
     it("should not allow user to get a refund while the campaign is active", async() => {
       await campaignInstance.contribute({from: alice, value: goal})
+      // If status is not expired it should revert
       await catchRevert(campaignInstance.getRefund({from: alice}))
     })
 
